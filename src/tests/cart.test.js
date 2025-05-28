b31abc00a55bed1171fdc88683735a0a2557df9e
Felix Skörvald <felix@skorvald.se>
2025-05-28 12:28:45 +0200

    123

--- src/tests/cart.test.js
+++ src/tests/cart.test.js
@@ -1,30 +1,33 @@
 // importera här
-import { addToCart, getCartItemCount } from "../cart"
+import { addToCart, getCartItemCount } from "../cart";
 
+describe("Cart", () => {
+    beforeEach(() => {
+        // Denna kod körs före varje test. Det är för att rensa kundvagnen, så inte saker ligger kvar från föregående test.
+        clearCart();
+    });
 
-describe('Cart', () => {
-	beforeEach(() => {
-		// Denna kod körs före varje test. Det är för att rensa kundvagnen, så inte saker ligger kvar från föregående test.
-		clearCart()
-	})
+    // -------------------------------------------------- //
+    // Skriv dina testfall här
+    test("getCartItemCount borde returnera noll när cart är tom", () => {
+        const expected = 0;
+        const actual = getCartItemCount();
 
+        expect(actual).toBe(expected);
+    });
 
-	// -------------------------------------------------- //
-	// Skriv dina testfall här
+    // Du får ett test att börja med
+    test("addToCart lägger till en ny produkt i kundvagnen", () => {
+        const itemCountBefore = getCartItemCount();
+        const input = { id: 1002, name: "Vattenpistol", price: 40 };
 
-	// Du får ett test att börja med
-	test('addToCart lägger till en ny produkt i kundvagnen', () => {
-		const itemCountBefore = getCartItemCount()
-		const input = { id: 1002, name: 'Vattenpistol', price: 40 }
+        // addToCart returnerar inget - den påverkar kundvagnen
+        // vi behöver använda getCartItemCount för att se om det har lagts till en ny produkt i kundvagnen
+        addToCart(input);
+        const itemCountAfter = getCartItemCount();
 
-		// addToCart returnerar inget - den påverkar kundvagnen
-		// vi behöver använda getCartItemCount för att se om det har lagts till en ny produkt i kundvagnen
-		addToCart(input)
-		const itemCountAfter = getCartItemCount()
+        expect(itemCountAfter).toBe(itemCountBefore + 1);
+    });
 
-		expect(itemCountAfter).toBe(itemCountBefore + 1)
-	})
-
-
-	// -------------------------------------------------- //
-})
+    // -------------------------------------------------- //
+});
